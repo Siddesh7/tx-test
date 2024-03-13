8e0890567643e1d856e55c82a5185ed79dfea157
Siddesh <siddeshsankhya@gmail.com>
2024-03-13 16:56:06 +0530

    cosmetic changes

--- api/index.tsx
+++ api/index.tsx
@@ -25,6 +25,7 @@ app.frame("/", (c) => {
           display: "flex",
           width: "100%",
           height: "100%",
+          fontSize: "60px",
           justifyContent: "center",
           alignItems: "center",
           backgroundColor: "black",
